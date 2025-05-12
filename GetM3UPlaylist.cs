3ced66ed59960092b5a4f2e378397a462ca64497
David Driscoll <david.driscoll@gmail.com>
2025-05-11 23:30:08 -0400

    weird

--- GetM3UPlaylist.cs
+++ GetM3UPlaylist.cs
@@ -5,58 +5,65 @@ using Microsoft.Extensions.Options;
 
 public static partial class GetM3UPlaylist
 {
-  public record Request() : IRequest<M3U>;
+    public record Request() : IRequest<M3U>;
 
-  class Handler(
-    IOptions<TiviOptions> options,
-    ILogger<Handler> logger) : IRequestHandler<Request, M3U>
-  {
-    public async Task<M3U> Handle(Request request, CancellationToken cancellationToken)
+    class Handler(
+        IOptions<TiviOptions> options,
+        ILogger<Handler> logger) : IRequestHandler<Request, M3U>
     {
-      var cacheDirectory = options.Value.CacheDirectory;
-      var fileName = $"{options.Value.Hostname}.m3u";
-      var filePath = Path.Combine(cacheDirectory, fileName);
-      try
-      {
-        using var client = new HttpClient();
-        client.Timeout = TimeSpan.FromMinutes(3);
-        var url = new UriBuilder()
+        public async Task<M3U> Handle(Request request, CancellationToken cancellationToken)
         {
-          Scheme = "http",
-          Path = "get.php",
-          Query = $"username={options.Value.Username}&password={options.Value.Password}&type=m3u_plus&output=ts",
-          Host = options.Value.Hostname,
-        };
+            var cacheDirectory = options.Value.CacheDirectory;
+            var fileName = $"{options.Value.Hostname}.m3u";
+            var filePath = Path.Combine(cacheDirectory, fileName);
+            try
+            {
+                using var client = new HttpClient();
+                client.Timeout = TimeSpan.FromMinutes(3);
+                var url = new UriBuilder()
+                {
+                    Scheme = "http",
+                    Path = "get.php",
+                    Query =
+                        $"username={options.Value.Username}&password={options.Value.Password}&type=m3u_plus&output=ts",
+                    Host = options.Value.Hostname,
+                };
 
-        var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url.Uri), cancellationToken);
-        if (!response.IsSuccessStatusCode)
-        {
-          logger.LogCritical("Unable to download m3u file {FilePath} ({StatusCode} {Reason})", filePath, response.StatusCode, response.ReasonPhrase);
-          throw new RequestFailedException($"Unable to download m3u file {filePath}");
+                logger.LogInformation("Downloading m3u file {FilePath} from {Url}", filePath, url.Uri);
+
+                var response =
+                    await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url.Uri), cancellationToken);
+                if (!response.IsSuccessStatusCode)
+                {
+                    logger.LogCritical("Unable to download m3u file {Url} ({StatusCode} {Reason})", url.Uri,
+                        response.StatusCode, response.ReasonPhrase);
+                    logger.LogCritical("Response: {Response}",
+                        await response.Content.ReadAsStringAsync(cancellationToken));
+                    throw new RequestFailedException($"Unable to download m3u file {url.Uri}");
+                }
+
+                var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+                if (!Directory.Exists(cacheDirectory))
+                {
+                    Directory.CreateDirectory(cacheDirectory);
+                }
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                await using var fileStream = File.Create(filePath);
+                await stream.CopyToAsync(fileStream, cancellationToken);
+            }
+            catch when (!File.Exists(filePath))
+            {
+                logger.LogCritical("Unable to download m3u file {Url}", filePath);
+                throw new RequestFailedException($"Unable to download m3u file {filePath}");
+            }
+
+            return M3UManager.M3UManager.ParseFromFile(filePath);
         }
-
-        var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
-
-        if (!Directory.Exists(cacheDirectory))
-        {
-          Directory.CreateDirectory(cacheDirectory);
-        }
-
-        if (File.Exists(filePath))
-        {
-          File.Delete(filePath);
-        }
-
-        await using var fileStream = File.Create(filePath);
-        await stream.CopyToAsync(fileStream, cancellationToken);
-      }
-      catch when (!File.Exists(filePath))
-      {
-        logger.LogCritical("Unable to download m3u file {FilePath}", filePath);
-        throw new RequestFailedException($"Unable to download m3u file {filePath}");
-      }
-
-      return M3UManager.M3UManager.ParseFromFile(filePath);
     }
-  }
 }
--- GetXmlTvPlaylist.cs
+++ GetXmlTvPlaylist.cs
@@ -5,59 +5,62 @@ using tivi.Models;
 
 public static partial class GetXmlTvPlaylist
 {
-  public record Request() : IRequest<Tv>;
+    public record Request() : IRequest<Tv>;
 
-  class Handler(
-    IOptions<TiviOptions> options,
-    ILogger<Handler> logger) : IRequestHandler<Request, Tv>
-  {
-    public async Task<Tv> Handle(Request request, CancellationToken cancellationToken)
+    class Handler(
+        IOptions<TiviOptions> options,
+        ILogger<Handler> logger) : IRequestHandler<Request, Tv>
     {
-      var cacheDirectory = options.Value.CacheDirectory;
-      var fileName = $"{options.Value.Hostname}.xml";
-      var filePath = Path.Combine(cacheDirectory, fileName);
-      try
-      {
-        using var client = new HttpClient();
-        client.Timeout = TimeSpan.FromMinutes(3);
-        var url = new UriBuilder()
+        public async Task<Tv> Handle(Request request, CancellationToken cancellationToken)
         {
-          Scheme = "http",
-          Path = "xmltv.php",
-          Query = $"username={options.Value.Username}&password={options.Value.Password}",
-          Host = options.Value.Hostname,
-        };
+            var cacheDirectory = options.Value.CacheDirectory;
+            var fileName = $"{options.Value.Hostname}.xml";
+            var filePath = Path.Combine(cacheDirectory, fileName);
+            try
+            {
+                using var client = new HttpClient();
+                client.Timeout = TimeSpan.FromMinutes(3);
+                var url = new UriBuilder()
+                {
+                    Scheme = "http",
+                    Path = "xmltv.php",
+                    Query = $"username={options.Value.Username}&password={options.Value.Password}",
+                    Host = options.Value.Hostname,
+                };
 
 
-        var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url.Uri), cancellationToken);
-        if (!response.IsSuccessStatusCode)
-        {
-          logger.LogCritical("Unable to download xmltv file {FilePath} ({StatusCode} {Reason})", filePath,
-            response.StatusCode, response.ReasonPhrase);
-          throw new RequestFailedException($"Unable to download m3u file {filePath}");
+                var response =
+                    await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url.Uri), cancellationToken);
+                if (!response.IsSuccessStatusCode)
+                {
+                    logger.LogCritical("Unable to download xmltv file {Url} ({StatusCode} {Reason})", url.Uri,
+                        response.StatusCode, response.ReasonPhrase);
+                    logger.LogCritical("Response: {Response}",
+                        await response.Content.ReadAsStringAsync(cancellationToken));
+                    throw new RequestFailedException($"Unable to download xmltv file {url.Uri}");
+                }
+
+                var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+                if (!Directory.Exists(cacheDirectory))
+                {
+                    Directory.CreateDirectory(cacheDirectory);
+                }
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                await using var fileStream = File.Create(filePath);
+                await stream.CopyToAsync(fileStream, cancellationToken);
+            }
+            catch when (!File.Exists(filePath))
+            {
+                throw new RequestFailedException($"Unable to download xmltv file {filePath}");
+            }
+
+            return Tv.Load(filePath);
         }
-
-        var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
-
-        if (!Directory.Exists(cacheDirectory))
-        {
-          Directory.CreateDirectory(cacheDirectory);
-        }
-
-        if (File.Exists(filePath))
-        {
-          File.Delete(filePath);
-        }
-
-        await using var fileStream = File.Create(filePath);
-        await stream.CopyToAsync(fileStream, cancellationToken);
-      }
-      catch when (!File.Exists(filePath))
-      {
-        throw new RequestFailedException($"Unable to download m3u file {filePath}");
-      }
-
-      return Tv.Load(filePath);
     }
-  }
 }
